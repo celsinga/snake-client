81959d7b6f727fef07a41a73a83fd480ebe8aad2
Colin <colinelsinga@gmail.com>
2020-12-02 23:03:49 +0000

    ove

--- client.js
+++ client.js
@@ -11,10 +11,25 @@ const connect = function() {
   conn.on('connect', () => {
     conn.write('Name: CME');
     console.log('Connected to game server successfully');
+    let index = 0;
+    let max = 35;
+    function recursion(index) {
+      if (index >= max) {
+        return conn;
+      }
+      setTimeout(() => {
+        // conn.write('Move: up');
+        recursion(index = index + 1);
+      }, 50);
+    } 
+    recursion(index);
   });
+  // conn.on('connect', () => {
+  //   conn.write('Move: up');
+  // })
   // interpret incoming data as text
   conn.on('data', (data) => {
-  console.log('Message from server:', data)
+    console.log('Message from server:', data)
   });
   return conn;
 };
