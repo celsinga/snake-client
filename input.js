79814d1927fa6da14fb601956e6d4e6a7c6ba56e
Colin <colinelsinga@gmail.com>
2020-12-03 00:44:54 +0000

    input added

---
+++ input.js
@@ -0,0 +1,18 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = function () {
+    stdin.on('data', (data) => {
+      if (data === '\u0003') {
+        process.exit();
+      }
+    });
+  }
+  handleUserInput(stdin);
+  return stdin;
+}
+
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,23 +1,25 @@
 const connect = require('./client');
+const setupInput = require('./input');
 
 console.log('Connecting ...');
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = function () {
-    stdin.on('data', (data) => {
-      if (data === '\u0003') {
-        process.exit();
-      }
-    });
-  }
-  handleUserInput(stdin);
-  return stdin;
-}
-
 setupInput();
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
+//   const handleUserInput = function () {
+//     stdin.on('data', (data) => {
+//       if (data === '\u0003') {
+//         process.exit();
+//       }
+//     });
+//   }
+//   handleUserInput(stdin);
+//   return stdin;
+// }
+
+// setupInput();
 
